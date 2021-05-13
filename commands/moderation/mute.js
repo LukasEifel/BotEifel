a0796a95967b4ba63892fa531319cbb5221cc99c
LukasEifel <64480549+LukasEifel@users.noreply.github.com>
2021-05-13 12:33:03 +0200

    Update mute and unmute

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,14 +1,16 @@
 module.exports = {
     name: 'mute',
-    permissions: 'MANAGE_ROLES',
+    permissions: 'MANAGE_GUILD',
+    asliases: ['m'],
+    usage: '[mention]',
     execute(message, args) {
         if (!message.mentions.users.size) {
             return message.reply(`you need to tag a user in order to mute them!`);
         }
 
-        const role = guild.roles.cache.find(role => role.name === 'Talkrecht');
+        const role = message.guild.roles.cache.find(role => role.name === 'Talkrecht');
         const taggedUser = message.mentions.users.first();
-        
+
         if(taggedUser.roles.cache.some(role => role.name === 'Talkrecht')) {
             taggedUser.roles.remove(role);
         } else {
--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -1,14 +1,16 @@
 module.exports = {
     name: 'unmute',
-    permissions: 'MANAGE_ROLES',
+    permissions: 'MANAGE_GUILD',
+    asliases: ['um'],
+    usage: '[mention]',
     execute(message, args) {
         if (!message.mentions.users.size) {
             return message.reply(`you need to tag a user in order to unmute them!`);
         }
 
-        const role = guild.roles.cache.find(role => role.name === 'Talkrecht');
+        const role = message.guild.roles.cache.find(role => role.name === 'Talkrecht');
         const taggedUser = message.mentions.users.first();
-        
+
         if(!taggedUser.roles.cache.some(role => role.name === 'Talkrecht')) {
             taggedUser.roles.add(role);
         } else {
