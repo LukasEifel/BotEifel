b1a96e183f5e57901eac750a49c142c17b5c691c
LukasEifel <64480549+LukasEifel@users.noreply.github.com>
2021-05-13 12:26:37 +0200

    Bug fix

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,23 +1,47 @@
+const { MessageEmbed } = require('discord.js');
+const db = require('quick.db');
+
 module.exports = {
     name: 'kick',
     description: 'returns boop',
     guildOnly: true,
-    permissions: 'KICK_MEMBERS',
-    execute(message, args, Discord) {
-        if (!message.mentions.users.size) {
-            return message.reply(`you need to tag a user in order to kick them!`);
-        }
-        const target = message.guild.member(message.mentions.users.first());
-        let kReason = args.join(' ').slice(22);
-        if(!message.member.hasPermission("KICK_MEMBERS"))
-          return message.reply('you don\'t have the permission to kick members!');
-        let kickEmbed = new Discord.MessageEmbed()
-          .setDescription('Kick')
-          .setColor('#ff750c')
-          .setThumbnail(target.user.avatarURL())
-          .setField(`User: ${target.username}, ID: ${target.id}`);
-        message.guild.member(target).kick('FORCE!');
-        message.delete();
-        message.channel.send(kickEmbed);
+    permissions: 'ADMINISTRATOR',
+    usage: '[mention]',
+    execute(message, args) {
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send('**You do not have permissions to kick members! - [KICK_MEMBERS]**');
+        if (!message.guild.me.hasPermission('KICK_MEMBERS')) return message.channel.send('**I do not have permissions to kick members! - [KICK_MEMBERS]**');
+
+        if (!args[0]) return message.channel.send('**Enter a user to kick!**');
+
+        var kickMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!kickMember) return message.channel.send('**User is not on the server!**');
+
+        if (kickMember.id === message.member.id) return message.channel.send('**You cannot kick yourself! IDIOT!**');
+
+        if (kickMember.user.bot) return message.channel.send('**Cannot kick a bot!**');
+        if (!kickMember.kickable) return message.channel.send('**Cannot kick this user!**');
+
+        var reason = args.slice(1).join(' ');
+        try {
+          const sembed2 = new MessageEmbed()
+            .setColor('RED')
+            .setField('**You habe been kicked from ${message.guild.name}**')
+            .setFooter(message.guild.name, message.guild.iconURL());
+          kickMember.send(sembed2).then(() =>
+            kickMember.kick()).catch(() => null);
+          } catch {
+            kickMember.kick();
+          }
+          if (reason) {
+            var sembed = new MessageEmbed()
+              .setColor('GREEN')
+              .setDescription('**${kickMember.user.username}** has been kicked for ${reason}!');
+            message.channel.send(sembed);
+          } else {
+            var sembed2 = new MessageEmbed()
+              .setColor('GREEN')
+              .setDescription('**${kickMember.user.username}** has been kicked!');
+            message.channel.send(sembed2);
+          }
     },
 };
