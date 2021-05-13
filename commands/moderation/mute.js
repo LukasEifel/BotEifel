56384ddb5a39fe747066d1758dd0c02bad763177
LukasEifel <64480549+LukasEifel@users.noreply.github.com>
2021-05-13 12:49:52 +0200

    Bug fix

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,3 +1,5 @@
+const db = require('quick.db');
+
 module.exports = {
     name: 'mute',
     permissions: 'MANAGE_GUILD',
@@ -8,8 +10,38 @@ module.exports = {
             return message.reply(`you need to tag a user in order to mute them!`);
         }
 
+        let muterole;
+        dbmute = db.fetch('muterole_${message.guild.id}');
         let role = message.guild.roles.cache.find(role => role.name === 'Talkrecht');
         let taggedUser = message.mentions.users.first();
+
+        if (!message.guild.roles.cache.has(dbmute)) {
+          muterole = role;
+        } else {
+          muterole = message.guild.roles.cache.has(dbmute);
+        }
+
+        if (!muterole) {
+              try {
+                  muterole = message.guild.roles.create({
+                      data: {
+                          name: "Talkrecht",
+                          color: "#514f48",
+                          permissions: []
+                      }
+                  })
+                  message.guild.channels.cache.forEach(async (channel) => {
+                      await channel.createOverwrite(muterole, {
+                          SEND_MESSAGES: false,
+                          ADD_REACTIONS: false,
+                          SPEAK: false,
+                          CONNECT: false,
+                      })
+                  })
+              } catch (e) {
+                  console.log(e);
+              }
+        };
 
         if(taggedUser.roles.cache.some(role => role.name === 'Talkrecht')) {
             taggedUser.roles.remove(role.id);
--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -1,3 +1,5 @@
+const db = require('quick.db');
+
 module.exports = {
     name: 'unmute',
     permissions: 'MANAGE_GUILD',
@@ -8,10 +10,40 @@ module.exports = {
             return message.reply(`you need to tag a user in order to unmute them!`);
         }
 
+        let muterole;
+        dbmute = db.fetch('muterole_${message.guild.id}');
         let role = message.guild.roles.cache.find(role => role.name === 'Talkrecht');
         let taggedUser = message.mentions.users.first();
 
-        if(!taggedUser.roles.cache.some(role => role.name === 'Talkrecht')) {
+        if (!message.guild.roles.cache.has(dbmute)) {
+          muterole = role;
+        } else {
+          muterole = message.guild.roles.cache.get(dbmute);
+        }
+
+        if (!muterole) {
+              try {
+                  muterole = message.guild.roles.create({
+                      data: {
+                          name: "Talkrecht",
+                          color: "#514f48",
+                          permissions: []
+                      }
+                  })
+                  message.guild.channels.cache.forEach(async (channel) => {
+                      await channel.createOverwrite(muterole, {
+                          SEND_MESSAGES: false,
+                          ADD_REACTIONS: false,
+                          SPEAK: false,
+                          CONNECT: false,
+                      })
+                  })
+              } catch (e) {
+                  console.log(e);
+              }
+        };
+
+        if(!taggedUser.roles.cache.has(role.id)) {
             taggedUser.roles.add(role.id);
         } else {
             message.reply(`${taggeduser.username} is already unmuted!`);
