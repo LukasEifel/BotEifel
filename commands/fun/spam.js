e089f1dcfa8275affc392e8ca8dd395d50c2fda9
LukasEifel <64480549+LukasEifel@users.noreply.github.com>
2021-07-29 17:18:17 +0200

    Update spam.js

--- commands/fun/spam.js
+++ commands/fun/spam.js
@@ -13,7 +13,10 @@ module.exports = {
 
         let user = msg.mentions.users.first();
 
-        if (user.id === '416248905450389507') return msg.reply('i cannot spam my developer.');
+        if (user.id === '416248905450389507') {
+            msg.reply('i cannot spam my developer. My great creator!');
+            user = msg.author;
+        }
 
         if (args.length === 2) {
             for (i = 0; i < parseInt(args[0]); i++) {
@@ -29,6 +32,11 @@ module.exports = {
                 msg.client.users.cache.get(args[1].replace(/[^0-9]/g, '')).send(spamMsg);
             }
         }
-        msg.channel.send(`${msg.mentions.users.first().username} wird gespammt. Herzlichen Glückwunsch!`);
+
+        if (user.id === '416248905450389507') {
+            msg.channel.send(`${msg.author.username} wird gespammt. Nicht meine Schuld!`);
+        } else {
+            msg.channel.send(`${msg.mentions.users.first().username} wird gespammt. Herzlichen Glückwunsch!`);
+        }
     },
 };
