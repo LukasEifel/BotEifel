0963db5135d61db52841889aa96e04429f1c17e0
LukasEifel <64480549+LukasEifel@users.noreply.github.com>
2021-07-29 19:24:10 +0200

    Fix

--- commands/fun/spam.js
+++ commands/fun/spam.js
@@ -2,11 +2,11 @@ const { messageEmbed, SystemChannelFlags } = require('discord.js');
 
 module.exports = {
     name: 'spam',
-    description: 'Spam a discord user of your choice',
+    description: 'Spam a discord user of your choice.',
     guildOnly: false,
     usage: '[amount] [mention]',
     args: true,
-    cooldown: 0,
+    cooldown: 10,
     execute(msg, args) {
         //console.log("[DEBUG] args[0]: " + args[0]);
         //console.log("[DEBUG] args[1]: " + args[1]);
@@ -15,6 +15,9 @@ module.exports = {
 
         if (user.id === '416248905450389507') {
             msg.reply('i cannot spam my developer. My great creator!');
+            user = msg.author;
+        } else if (user.id === msg.client.user.id) {
+            msg.reply('i will not spam myself.');
             user = msg.author;
         }
 
@@ -31,7 +34,7 @@ module.exports = {
             console.log(`[BOT] sent to ${user.username}: ${spamMsg}`);
         }
 
-        if (user.id === '416248905450389507') {
+        if (msg.mentions.users.first().id === '416248905450389507') {
             msg.channel.send(`${msg.author.username} wird gespammt. Nicht meine Schuld!`);
         } else {
             msg.channel.send(`${msg.mentions.users.first().username} wird gespammt. Herzlichen Glückwunsch!`);
--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,4 @@
-const { prefix } = require('../../config.json');
+const prefix = require('discord-prefix');
 
 module.exports = {
     name: 'help',
@@ -10,20 +10,20 @@ module.exports = {
         const data = [];
         const { commands } = message.client;
 
+        let guildPrefix;
+        try {
+            guildPrefix = prefix.getPrefix(msg.guild.id);
+            if (!guildPrefix) guildPrefix = prefix.getPrefix();
+        } catch {
+            guildPrefix = prefix.getPrefix();
+        }
+
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`{prefix}help [command name]\` to get info on a specific command!`);
 
-            return message.author.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type == 'dm') return;
-                    message.reply('I\'ve sent you a PM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t PM you! Do you have PMs disabled?');
-                })
+            return message.channel.send(data, { split: true })
         }
 
         const name = args[0].toLowerCase();
@@ -37,7 +37,7 @@ module.exports = {
 
         if(command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
         if(command.description) data.push(`**Description:** ${command.description}`);
-        if(command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if(command.usage) data.push(`**Usage:** ${guildPrefix}${command.name} ${command.usage}`);
 
         data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
